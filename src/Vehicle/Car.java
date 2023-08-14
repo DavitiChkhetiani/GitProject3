8a74d30ccb45ab5dcaffd45dcc0aedfc87ad22cc
DavitiChkhetiani <davitichkhetiani@gmail.com>
2023-08-14 13:29:58 -0400

    getters and setters are added to car and plae.

--- src/Vehicle/Car.java
+++ src/Vehicle/Car.java
@@ -1,9 +1,21 @@
 package Vehicle;
 
 public class Car {
+    String make;
+    String model;
+    int year;
+
     public static void main(String[] args) {
         for (int i=0; i<10; i++){
             System.out.println("Car goes");
         }
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
     }
 }
--- src/Vehicle/Plane.java
+++ src/Vehicle/Plane.java
@@ -4,4 +4,8 @@ public class Plane {
     public static void main(String[] args) {
         System.out.println("plain flies");
     }
+    public void method1(){
+        System.out.println("Plane goes fast");
+
+    }
 }
